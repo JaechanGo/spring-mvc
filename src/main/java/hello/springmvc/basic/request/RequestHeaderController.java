66f7cb4cd1c17ea39c9e1f1cbc12f2b0d608cbbc
jaechango_cudo <happypuki@gmail.com>
2022-01-21 10:49:46 +0900

    Spring-mvc Ver_1.0 - 2022.01.21

--- src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
+++ src/main/java/hello/springmvc/basic/request/RequestHeaderController.java
@@ -5,19 +5,36 @@ import org.apache.catalina.util.Strftime;
 import org.apache.tomcat.jni.Local;
 import org.springframework.http.HttpMethod;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 @RestController
 @Slf4j
 public class RequestHeaderController {
     @RequestMapping("/headers")
-    public String headers(HttpServletRequest request, HttpServletResponse response, HttpMethod httpMethod, Local local, @RequestHeader MultiValueMap<String, String> headerMap,
+    public String headers(HttpServletRequest request,
+                          HttpServletResponse response,
+                          HttpMethod httpMethod,
+                          Locale locale,
+                          @RequestHeader MultiValueMap<String, String> headerMap,
+                          @RequestHeader ("host")String host,
+                          @CookieValue (value="myCookie",required = false) String cookie
                           ){
+
+        log.info("request={}", request);
+        log.info("response={}", response);
+        log.info("httpMethod={}", httpMethod);
+        log.info("locale={}", locale);
+        log.info("headerMap={}", headerMap);
+        log.info("header host={}", host);
+        log.info("myCookie={}", cookie);
+        return "ok";
 
     }
 }
